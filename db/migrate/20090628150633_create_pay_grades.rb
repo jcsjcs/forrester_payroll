d2d54499baad3da3c64c03f80c9f9f90edd3bcb5
Rainer Thiel <rainerthiel@zoho.com>
2009-06-30 22:01:01 +0200

    modified pay_grades migration

--- db/migrate/20090628150633_create_pay_grades.rb
+++ db/migrate/20090628150633_create_pay_grades.rb
@@ -1,11 +1,15 @@
 class CreatePayGrades < ActiveRecord::Migration
   def self.up
     create_table :pay_grades do |t|
-      t.integer :pay_grade
-      t.string :description
-
+      t.string :pay_grade, :null => false
+      t.string :description, :null => true
+      t.timestamp :valid_from, :null => false
+      t.timestamp :valid_until, :null => false
       t.timestamps
+      t.string :created_by, :null => false
+      t.string :updated_by, :null => true
     end
+    add_index :pay_grades, [:pay_grade, :valid_from], :unique => true
   end
 
   def self.down
